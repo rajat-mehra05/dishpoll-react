2c14bbde641a3a0a388e4452791d91adcc174252
rajat-mehra05 <rajat.m.5278@gmail.com>
2022-09-14 10:04:38 +0530

    fix: login btn css

--- src/components/login/Button.jsx
+++ src/components/login/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ children, ...props }) => {
   return (
     <button
-      className="text-xl bg-gradient-to-r from-[#a00e0e] to-[#815644] hover:from-[#7e2b08] hover:to-[#960f0f] font-semibold text-white py-2 px-4 rounded"
+      className="text-xl bg-gradient-to-r from-orange-600 to-orange-900 hover:from-[#7e2b08] hover:to-[#960f0f] font-semibold text-white py-2 px-4 rounded"
       {...props}
     >
       {children}
